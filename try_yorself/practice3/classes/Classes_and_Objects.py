7d8d38cd297c593d03a6f63d3531f514409faccf
nikepr0 <posses@gmail.com>
2026-02-21 12:38:08 +0500

    w3school

---
+++ try_yorself/practice3/classes/Classes_and_Objects.py
@@ -0,0 +1,21 @@
+
+#To create a class, use the keyword class:
+
+#You can delete objects by using the del keyword:
+#del p1 
+
+#Create three objects from the MyClass class:
+"""p1 = MyClass()
+p2 = MyClass()
+p3 = MyClass()
+
+print(p1.x)
+print(p2.x)
+print(p3.x) 
+"""
+
+
+#class definitions cannot be empty, but if you for some reason have a class definition with no content, put in the pass statement to avoid getting #an error.
+#Example
+class Person:
+  pass 
